067e952a73717fca4ad3662979ce0f6e12dc000a
agrajak <korbots@gmail.com>
2018-09-01 03:35:37 +0900

    알림간다잉

---
+++ app/src/main/java/kr/inha/ice/smartcanine/TimePickerActivity.java
@@ -0,0 +1,13 @@
+package kr.inha.ice.smartcanine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_picker);
+    }
+}
